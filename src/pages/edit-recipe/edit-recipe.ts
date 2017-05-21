eec5df9e28b820a77213080bd5ad2ea5cba6fa96
Yuttana <yuttana76>
2017-05-21 10:47:59 +0700

    Add ActionSheetController & AlertController

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,6 @@
-import { FormGroup ,FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, ActionSheetController, IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
 @IonicPage()
@@ -9,27 +9,91 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edit-recipe.html',
 })
 export class EditRecipe implements OnInit {
-  
-  mode = 'New';
-  selectOptions=['Easy','Medium','Hard'];
-  recipeForm : FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public actionSheetController: ActionSheetController, 
+              public alertCtrl: AlertController) { }
 
   ngOnInit(): void {
     this.mode = this.navParams.get('mode');
     this.initializeForm();
   }
 
-onSubmit(){
-console.log(this.recipeForm);
-}
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 
-  private initializeForm(){
+  private initializeForm() {
     this.recipeForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
       'description': new FormControl(null, Validators.required),
       'difficulty': new FormControl('Medium', Validators.required),
+      'ingredients':new FormArray([]),
+    });
+  }
+
+  onManageIngredients() {
+    const actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredient',
+          handler: () => {
+            this.createNewIngredientAlert().present();
+          }
+        },
+        {
+          text: 'Remove all ingredients',
+          role: 'destructive',
+          handler: () => {
+            //
+          }
+
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+
+    });
+
+    actionSheet.present();
+    
+  }
+
+  private createNewIngredientAlert() {
+
+    return this.alertCtrl.create({
+      title: 'Add Ingredient',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },{
+          text:'Add',
+          handler:data =>{
+
+            if(data.name == null ||data.name.trim() ==''){
+                return;
+            }
+
+            (<FormArray>this.recipeForm.get('ingredients'))
+            .push(new FormControl(data.name,Validators.required));
+
+          }
+        }
+      ]
     });
   }
 
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,13 +1,8 @@
 import { EditRecipe } from './../edit-recipe/edit-recipe';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
-/**
- * Generated class for the Recipes page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-recipes',
